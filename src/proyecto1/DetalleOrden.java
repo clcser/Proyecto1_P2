fbc396d0b19a2ae30b49d08be1850395ecc25366
Leonardo Lovera <leonard.lover@gmail.com>
2022-09-27 11:55:30 -0300

    Added skeleton for DetalleOrden and Pago

--- src/proyecto1/DetalleOrden.java
+++ src/proyecto1/DetalleOrden.java
@@ -8,6 +8,67 @@ package proyecto1;
  *
  * @author clau
  */
+class Articulo {
+	private float peso;
+	private String nombre;
+	private String descripcion;
+	private float precio;
+
+	public Articulo() {
+	}
+
+	public float getPeso() {
+	}
+
+	public void setPeso(float peso) {
+	}
+
+	public String getNombre() {
+	}
+
+	public void setNombre(String nombre) {
+	}
+
+	private String getDescripcion() {
+	}
+
+	private void setDescripcion(String descripcion) {
+	}
+
+	private float getPrecio() {
+	}
+
+	private void setPrecio(float precio) {
+	}
+
+	public String toString() {
+	}
+}
+
 public class DetalleOrden {
-    
+	private int cantidad;
+
+	public DetalleOrden() {
+	}
+
+	public int getCantidad() {
+	}
+
+	public void setCantidad(int cantidad) {
+	}
+
+	public String toString() {
+	}
+
+	public float calcPrecio() {
+	}
+
+	public float calcPrecioSinIVA() {
+	}
+
+	public float calcIVA() {
+	}
+
+	public float calcPeso() {
+	}
 }
--- src/proyecto1/Pago.java
+++ src/proyecto1/Pago.java
@@ -9,5 +9,72 @@ package proyecto1;
  * @author clau
  */
 public class Pago {
-    
+	private float monto;
+	private Date fecha;
+
+	public Pago () {
+	}
+
+	public float getMonto() {
+	}
+
+	public void setMonto(float monto) {
+	}
+
+	public Date getFecha() {
+	}
+
+	public void setFecha(Date fecha) {
+	}
+
+	public String toString() {
+	}
+}
+
+class Efectivo extends Pago {
+	public Efectivo () {
+	}
+
+	public float calcDevolucion() {
+	}
+}
+
+class Transferencia extends Pago {
+	private String banco;
+	private String numCuenta;
+
+	public Transferencia() {
+	}
+
+	public String getBanco() {
+	}
+
+	public void setBanco(String banco) {
+	}
+
+	public String getNumCuenta() {
+	}
+
+	public void setNumCuenta(String numCuenta) {
+	}
+}
+
+class Tarjeta extends Pago {
+	private String tipo;
+	private String numTransaccion;
+
+	public Tarjeta() {
+	}
+
+	public String getTipo() {
+	}
+
+	public void setTipo(String tipo) {
+	}
+
+	public String getNumTransaccion() {
+	}
+
+	public void setNumTransaccion(String numTransaccion) {
+	}
 }
